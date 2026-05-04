cf68190eaf9143ba2459e83048aee10bec6c95db
link-coder100788 <240818174+link-coder100788@users.noreply.github.com>
2026-05-04 16:04:42 -0700

    Formula is ready to test

--- Formula/ruby-sdl2.rb
+++ Formula/ruby-sdl2.rb
@@ -2,5 +2,36 @@
 # frozen_string_literal: true
 
 class RubySDL2 < Formula
-    desc "Python and ruby bindings for SDL2"
+    desc "Ruby bindings for SDL2"
+    homepage "https://github.com/link-coder100788/python-sdl2"
+    url "https://github.com/link-coder100788/python-sdl2/archive/refs/tags/0.0.1.tar.gz"
+    sha256 "33433bd8a41e7a322f933aa8649e7c9ec7e0250baaed6171627dbc572e1040dc"
+    licence "MIT"
+
+    depends_on "cmake" => :build
+    depends_on "sdl2"
+    depends_on "openal-soft"
+    depends_on "llvm"
+    depends_on "ruby"
+
+    def install
+        system "cmake", "--build", "/Users/noahpegna/code/python-sdl2/cmake-build-debug", "--target", "ruby_sdl2"
+        lib.install Dir["build/ruby_sdl2.bundle"]
+        (lib/"ruby").install "ruby_sdl2.rb"
+        (bin/"ruby_sdl2_env").write <<~EOS
+            #!/bin/bash
+            echo 'export RUBYLIB="#{opt_lib}/ruby:$RUBYLIB"'
+        EOS
+        chmod "+x", bin/"ruby_sdl2_env"
+    end
+
+    def caveats
+        <<~EOS
+            To use ruby_sdl2:
+
+                eval "$(ruby_sdl2_env)"
+                ruby -e 'require "ruby_sdl2"'
+        EOS
+    end
+end
 
